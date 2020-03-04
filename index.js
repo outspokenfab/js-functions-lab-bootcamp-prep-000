0fe9cc0e6e28f7f509d747f9e82444b83915ba34
curious-metric-5246 <richardjharvey1@gmail.com>
2020-03-04 14:04:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // write your code below!
 function happyHolidays (){
   return 'Happy holidays!'
-}
+};
 
 function happyHolidaysTo (name){
   console.log(`Happy holidays, ${name}!`)
-}
+};
 
 function happyCustomHolidayTo (holiday, name){
   console.log(`Happy ${holiday}, ${name}!`)
-}
+};
 
 function holidayCountdown (holiday, daysUntil){
   console.log(`It's ${daysUntil} days until ${holiday}!`)
-}
+};
 
 holidayCountdown("Thanksgiving",84);
